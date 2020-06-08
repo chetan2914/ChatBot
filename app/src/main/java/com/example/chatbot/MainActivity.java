bf9285d25fac0fd43d204997b56ec602cfc893ca
chetan <chetanc2914@gmail.com>
2020-06-08 15:12:01 +0530

    initial

--- app/src/main/java/com/example/chatbot/MainActivity.java
+++ app/src/main/java/com/example/chatbot/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,6 +15,7 @@ import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -47,6 +49,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ListView msglist;
     ArrayAdapter<String> arrayAdapter;
     private ArrayList<Message> arrayList = new ArrayList<Message>(100);
+    private ArrayList<String> buttonlist = new ArrayList<String>(100);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,18 +65,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         String strmsg = msg.getText().toString().trim();
+        msg.setText("");
+        requestbot(strmsg);
+    }
+    public void addbutton(ArrayList btnlist)
+    {
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        final LinearLayout verticalLayout= (LinearLayout)findViewById(R.id.buttonlayout);
+        verticalLayout.removeAllViews();
+        int numberOfRows =0;
+        int TotalButtons=btnlist.size();
+        int counter=0;
+        if(btnlist.size()%2==0){
+            numberOfRows=TotalButtons/2;
+        }
+        else {
+            numberOfRows=(TotalButtons/2)+1;
+        }
+        int numberOfButtonsPerRow =2;
+        int buttonIdNumber = 0;
+        for(int i=0;i<numberOfRows;i++){
+            LinearLayout newLine = new LinearLayout(this);
+            newLine.setLayoutParams(params);
+            newLine.setOrientation(LinearLayout.HORIZONTAL);
+            for(int j=0;j<numberOfButtonsPerRow;j++) {
+                if (counter < TotalButtons) {
+                    final Button button = new Button(this);
+                    // You can set button parameters here:
+                    params.weight=1;
+                    button.setTag(btnlist.get(buttonIdNumber).toString());
+                    button.setLayoutParams(params);
+                    button.setText(btnlist.get(buttonIdNumber).toString());//button text
+                    button.setOnClickListener(new View.OnClickListener() {
+                        public void onClick(View view) {
+                            requestbot(button.getTag().toString());
+                        }
+                    });
+                    newLine.addView(button);
+                    buttonIdNumber++;
+                    counter++;
+                }
+
+            }
+            verticalLayout.addView(newLine);
+        }
+    }
+    public void requestbot(String msg)
+    {
         RequestQueue requestQueue = Volley.newRequestQueue(this);
-        String url = "http://192.168.1.5:3000/api/v1/bots/bibhu/converse/userId";
+        String url = "http://192.168.1.8:3000/api/v1/bots/satyam/converse/userId";
         JSONObject jsonBody = new JSONObject();
         Message m=new Message();
-        m.setMessage(strmsg);
+        m.setMessage(msg);
         m.setSelf(true);
         arrayList.add(m);
         customadapter c=new customadapter(getApplicationContext(),arrayList);
         msglist.setAdapter(c);
         try {
             jsonBody.put("type", "text");
-            jsonBody.put("text", strmsg);
+            jsonBody.put("text", msg);
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -97,6 +148,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                     arrayList.add(m);
                                     customadapter d=new customadapter(getApplicationContext(),arrayList);
                                     msglist.setAdapter(d);
+                                }
+                                if(c.getString("type").equals("custom")){
+                                    buttonlist.clear();
+                                    JSONArray qr=c.getJSONArray("quick_replies");
+                                    for (int nr=0;nr<qr.length();nr++){
+                                        JSONObject obj=qr.getJSONObject(nr);
+                                        String btntext=obj.getString("title");
+                                        buttonlist.add(btntext);
+                                    }
+                                    Log.i("btnlist",buttonlist.toString());
+                                    addbutton(buttonlist);
                                 }
 
                             }
